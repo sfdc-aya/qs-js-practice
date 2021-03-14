5aec137b4c678ebf82916644c3eddcd7a5070c7c
Ayazhan <ayazhanmukhammedova@gmail.com>
2021-03-14 13:43:07 -0700

    added

--- 57.js
+++ 57.js
@@ -1,13 +1,9 @@
-function addToFront(arr, string) {
-    arr.push(string);
-    arr.unshift(string);
-    //console.log(arr);
-    arr.pop(string);
-    //console.log(arr);
-  
-  }
-  var output = addToFront([1, 2], 3);
-  console.log(output); // -> [3, 1, 2]
-  //var output = addToFront(["apple","banana"],"orange")
-  //["orange","apple","banana"]
-  //console.log(output);
+function addToFront(arr, element) {
+  arr.splice(0,0, element);
+  return arr;
+}
+var output = addToFront([1, 2], 3);
+console.log(output); // -> [3, 1, 2]
+var output = addToFront(["apple","banana"],"orange")
+//["orange","apple","banana"]
+console.log(output);
